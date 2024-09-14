beef6df7bbac7f99e9772ebe5019fff94521a73e
Jaime Ansorena <jansorenac@gmail.com>
2024-09-14 15:42:37 -0300

    final solution

--- FFMS-greedy.py
+++ FFMS-greedy.py
@@ -72,44 +72,14 @@ def main():
     drate = args.drate
     instance = args.instance
     
-    
     strings = read_instance(instance)
+    start_time = time.time()
     solution = greedy_ffmsp(strings, threshold, drate)
+    end_time = time.time()
     score = evaluate_solution(solution, strings, threshold)
-    print(f"Solution: {solution}")
+    #print(f"Solution: {solution}")
     print(f"Score: {score}")
-    
+    print(f"Time: {end_time - start_time:.12f}")
 
-    '''
-    # Leer la instancias del problema
-    score = []    
-    
-    path = 'datasets/'
-    
-    archivos_aux = glob.glob(os.path.join(path, "*.txt"))
-    archivos_filtrados = [archivo for archivo in archivos_aux if not archivo.endswith("readme.txt")]
-    
-    archivos_matrix = [ # (1,2)
-        ([archivo for archivo in archivos_filtrados if os.path.basename(archivo).startswith("100-300")],"100-300"),
-        ([archivo for archivo in archivos_filtrados if os.path.basename(archivo).startswith("100-600")],"100-600"),
-        ([archivo for archivo in archivos_filtrados if os.path.basename(archivo).startswith("100-800")],"100-800"),
-        ([archivo for archivo in archivos_filtrados if os.path.basename(archivo).startswith("200-300")],"200-300"),
-        ([archivo for archivo in archivos_filtrados if os.path.basename(archivo).startswith("200-600")],"200-600"),
-        ([archivo for archivo in archivos_filtrados if os.path.basename(archivo).startswith("200-800")],"200-800"),
-    ]
-    cont = 0
-    for archivos,nombre in archivos_matrix:
-        cont += 1
-        for archivo in archivos:
-            strings = read_instance(archivo)
-            solution = greedy_ffmsp(strings, threshold, drate)
-            score.append(evaluate_solution(solution, strings, threshold))
-        media = average(score,len(strings[0]))
-        print('-'*50)
-        print(f"dataset: {nombre}")
-        print(f"average: {media}")
-        print(f"standar deviation: {standar_deviation(score,average(score,len(strings[0])),len(strings[0]))}")
-        print('-'*50)
-    '''
 if __name__ == "__main__":
     main()
